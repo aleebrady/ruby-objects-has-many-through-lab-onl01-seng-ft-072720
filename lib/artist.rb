658293746629eebfb078b5ece06e3292e77ddbd4
Aaron Brady <aleebrady@gmail.com>
2020-08-13 16:29:28 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Artist 
+  
+  attr_accessor :name # initializes name
+  
+  @@all = []
+  
+  def initialize(name) 
+    @name = name  # initializes name 
+    @@all << self # will add new artist to array when created (artist = Artist.new(artist))
+  end
+  
+  def self.all # class method will add artist to class variable set to empty array
+    @@all
+  end
+  
+  def songs # returns song by a specific artist 
+    Song.all.select do |song| # get all songs fron Song class and self each one
+      song.artist == self # song.artist == artist of song 
+    end
+  end
+  
+  
+  
+end
